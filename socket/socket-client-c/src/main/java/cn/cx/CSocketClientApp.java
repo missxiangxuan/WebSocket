dccb50018827f97e5ee83d026eb0e3f8bd91bbf7
陈翔 <xiang3.chen@ly.com>
2019-11-11 14:56:02 +0800

    Socket提交

---
+++ socket/socket-client-c/src/main/java/cn/cx/CSocketClientApp.java
@@ -0,0 +1,36 @@
+package cn.cx;
+
+import cn.cx.socket.MyWebSocketClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author: cn.cn.cx
+ * @date : 2019/11/7 17:16
+ * @description:
+ */
+@SpringBootApplication
+public class CSocketClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CSocketClientApp.class,args);
+    }
+
+
+    @Bean
+    public MyWebSocketClient webSocketClient() {
+        try {
+            MyWebSocketClient webSocketClient = new MyWebSocketClient(new URI("ws://127.0.0.1:8001/ES/weixinWebSocket"));
+            webSocketClient.connect();
+            return webSocketClient;
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
