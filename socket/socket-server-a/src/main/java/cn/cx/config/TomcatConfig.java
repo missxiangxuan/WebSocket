b6dfd52c1fbeefc57c69108eda6fb860f43ba6be
陈翔 <xiang3.chen@ly.com>
2019-11-24 22:19:08 +0800

    cx update

---
+++ socket/socket-server-a/src/main/java/cn/cx/config/TomcatConfig.java
@@ -0,0 +1,46 @@
+package cn.cx.config;
+
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.websocket.server.WsSci;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
+    }
+
+    @Bean
+    public TomcatContextCustomizer tomcatContextCustomizer() {
+        System.out.println("init");
+        return new TomcatContextCustomizer() {
+            @Override
+            public void customize(Context context) {
+                System.out.println("init   customize");
+                context.addServletContainerInitializer(new WsSci(), null);
+            }
+
+        };
+
+    }
+
+}
